048236bbf05c4a5a3d4aa93e8cd7c35586f706bf
VladPr <vp@rodeapps.com>
2025-03-05 13:21:55 +0100

    Initial Commit

---
+++ src/test/java/com/pim/productinformationmanagement/ProductInformationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pim.productinformationmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInformationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
